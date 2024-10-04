ae8fdcf588a917c4ced8c06a26b267fb7af4206f
USER <USER@DESKTOP-S5OJSS9>
2024-10-04 09:48:41 +0900

    천지희) (생성) 임의 파일 생성 (push 확인 용)

--- thegrace/src/main/java/com/smhrd/controller/JoinController.java
+++ thegrace/src/main/java/com/smhrd/controller/JoinController.java
@@ -15,28 +15,6 @@ public class JoinController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 요청 파라미터 받기 (아이디, 비밀번호, 닉네임 -> 한글(인코딩))
-		
-		request.setCharacterEncoding("utf-8");
-		
-		String id = request.getParameter("id");
-		String pw = request.getParameter("pw");
-		String nickname = request.getParameter("nickname");
-		
-		// 서버에 저장 - JDBC사용 가능하지만 더 사용하기 쉽고 명확한 
-		MavenMember joinMember = new MavenMember(id, pw, nickname);
-		
-		// myBatis DB 연동 프레임 워크 사용해서 테이블에 값 저장하기
-		// => 3가지 방법 : DAO(*), @(Annotation), Mapper Interface
-		MemberDAO dao = new MemberDAO();
-		int res = dao.join(joinMember);
-		
-		// 회원가입 성공 (res>0) => index.jsp / 실패 => join.html
-		if(res>0) {
-			response.sendRedirect("index.jsp");
-		} else {
-			response.sendRedirect("join.html");
-		}
+	
 	}
-
 }
--- thegrace/src/main/java/com/smhrd/controller/LoginController.java
+++ thegrace/src/main/java/com/smhrd/controller/LoginController.java
@@ -17,26 +17,11 @@ public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
 		
-		String id = request.getParameter("id");
-		String pw = request.getParameter("pw");
 		
-		MavenMember loginMember = new MavenMember(id, pw);
 		
-		MemberDAO dao = new MemberDAO();
-		// res is null => 로그인 실패 / null x => 성공
-		MavenMember res = dao.login(loginMember);
 		
-		if(res == null) {
-			System.out.println("로그인 실패");
-			response.sendRedirect("login.html");
-		} else {
-			System.out.println("로그인 성공 " + res.getNickname());
-			HttpSession session = request.getSession();
-			session.setAttribute("member", res);
-			response.sendRedirect("index.jsp");
-		}
+		
 	}
 
 }
