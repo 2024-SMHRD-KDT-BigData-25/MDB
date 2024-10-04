b256fde9b5b8e1b040c78b1fda24058d6109d068
smhrd <smhrd@DESKTOP-QJSBUE2>
2024-10-04 17:52:49 +0900

    유초원 두번째 커밋 팔로워, 팔로이 수와 유저가 작성한 리뷰들의 추천수 총합 계산(followpage.jsp)

--- thegrace/src/main/java/com/smhrd/model/MovieDAO.java
+++ thegrace/src/main/java/com/smhrd/model/MovieDAO.java
@@ -1,5 +1,7 @@
 package com.smhrd.model;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
@@ -15,9 +17,37 @@ public class MovieDAO {
 	public UserInfo login(UserInfo m) {
 		SqlSession sqlSession = sqlSessionFactory.openSession(true);
 		// 반환값 T ? => 임의읙 객체로 반환값을 나중에 우리가 지정한 형태로 받을 수 있음
-		UserInfo res = sqlSession.selectOne("MemberMapper.login", m);
+		UserInfo res = sqlSession.selectOne("MovieMapper.login", m);
 		sqlSession.close();
 		return res;
+	}
+	
+	public String followerCnt(String user_email) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		String res = sqlSession.selectOne("MovieMapper.followerCnt", user_email);
+		sqlSession.close();
+		return res;
+	}
+	
+	public String followeeCnt(String user_email) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		String res = sqlSession.selectOne("MovieMapper.followeeCnt", user_email);
+		sqlSession.close();
+		return res;
+	}
+	
+	public int allReviewRecmCnt(String user_email) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		List<ReviewInfo> reviewList = sqlSession.selectList("MovieMapper.reviewList", user_email);
+		int res = 0;
+		for ( ReviewInfo m : reviewList ) {
+			Double review_cd = m.getReview_cd();
+			int reviewCnt = Integer.parseInt(sqlSession.selectOne("MovieMapper.reviewRecmCnt", review_cd));
+			res += reviewCnt;
+		}
+		sqlSession.close();
+		return res;
+		
 	}
 
 }
