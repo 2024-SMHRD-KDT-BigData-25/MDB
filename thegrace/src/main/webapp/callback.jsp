1b91fd2e6de06a2eba4e96f6cf1593f91043a3e2
smhrd <smhrd@DESKTOP-QJSBUE2>
2024-10-08 14:55:56 +0900

    유초원) (수정) login.html login.js MovieMapper 로그인 시 이메일 및 비밀번호 입력 요청 문구 출력 기능 구현을 위한 수정(중간)

--- thegrace/src/main/webapp/callback.jsp
+++ thegrace/src/main/webapp/callback.jsp
@@ -1,5 +1,5 @@
 
-<%@page import="org.json.JSONObject"%>
+<%@page import="org.json.*"%>
 <%@page import="org.apache.ibatis.reflection.SystemMetaObject"%>
 <%@ page import="java.net.URLEncoder" %>
 <%@ page import="java.net.URL" %>
--- thegrace/src/main/webapp/resources/js/login.js
+++ thegrace/src/main/webapp/resources/js/login.js
@@ -1,7 +1,9 @@
-
 const loginId = document.getElementById('input_email');
 const loginPw = document.getElementById('input_pw');
 const loginBtn = document.getElementById('LOGIN_BTN');
+const emailWarning = document.getElementById('email-warning'); // 경고 메시지 요소
+const passwordWarning = document.getElementById('password-warning'); // 경고 메시지 요소
+
 
 function color() {
     if((loginId.value.length>0 && loginId.value.indexOf("@")!==-1) 
@@ -16,6 +18,22 @@ function color() {
 
 function moveToMain(){
     location.replace("./main.html");
+}
+
+function checkInputs() {
+    // 이메일 검증
+    if (loginId.value.length === 0) {
+        emailWarning.style.display = 'block'; // 경고 메시지 보이기
+    } else {
+        emailWarning.style.display = 'none'; // 경고 메시지 숨기기
+    }
+
+    // 비밀번호 검증
+    if (loginPw.value.length === 0) {
+        passwordWarning.style.display = 'block'; // 경고 메시지 보이기
+    } else {
+        passwordWarning.style.display = 'none'; // 경고 메시지 숨기기
+    }
 }
 
 loginId.addEventListener('keyup', color);
