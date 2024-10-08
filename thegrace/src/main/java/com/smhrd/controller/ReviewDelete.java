de8879d883e42bb06ea6ee58e32cfb22d07df357
smhrd <smhrd@smart>
2024-10-08 16:36:14 +0900

    김민규) (수정) 게시판관련 파일들 : 이름 수정하고 코드 조금 작성했습니다

--- thegrace/src/main/java/com/smhrd/controller/ReviewDelete.java
+++ thegrace/src/main/java/com/smhrd/controller/ReviewDelete.java
@@ -7,26 +7,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class BoardWrite
- */
-@WebServlet("/write")
-public class BoardWrite extends HttpServlet {
+@WebServlet("/delete")
+public class ReviewDelete extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BoardWrite() {
+    public ReviewDelete() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
 }
