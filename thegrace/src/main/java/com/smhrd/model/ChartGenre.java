6a19defcd919824975c3f2ee38cd6db759f1dec1
USER <USER@DESKTOP-S5OJSS9>
2024-10-16 10:52:50 +0900

    천지희) reviewShow.jsp, ChartGenre.java(VO)

---
+++ thegrace/src/main/java/com/smhrd/model/ChartGenre.java
@@ -0,0 +1,14 @@
+package com.smhrd.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ChartGenre {
+	    private int cnt;
+	    private String mv_genre;
+
+}
