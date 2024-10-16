515f331f5a58c44fba109913722a84ebc550d710
USER <USER@DESKTOP-S5OJSS9>
2024-10-16 11:33:53 +0900

    천지희) ChartGenre, MovieDAO, MovieMapper

--- thegrace/src/main/java/com/smhrd/model/ChartGenre.java
+++ thegrace/src/main/java/com/smhrd/model/ChartGenre.java
@@ -1,14 +1,22 @@
 package com.smhrd.model;
 
+import java.sql.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
+
 public class ChartGenre {
 	    private int cnt;
 	    private String mv_genre;
+	    
+		public ChartGenre(String name, int cnt) {
+			super();
+			this.mv_genre = name;
+			this.cnt = cnt;
+		}
 
 }
